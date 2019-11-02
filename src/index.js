b30ae537661fa9c4eae7cca3b4cc28dc16f6af27
Joey Killam <jhkillam@gmail.com>
2019-11-02 14:02:28 -0500

    completed basic exercises

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 const { createStore } = Redux;
 
 // Initialize the Redux store by passing it our reducer (defined globally in reducer.js)
-const { subscribe, dispatch, getState } = createStore(reducer);
+const { subscribe, dispatch, getState } = createStore(reducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 
 // Re-render the application every time the state changes
 // Here we pass the Redux state to our render method (defined globally in render.js)
@@ -10,3 +10,39 @@ subscribe(() => render(getState()))
 // Dispatch the "INCREMENT" action every time the +1 button is pressed
 const incrementButton = document.getElementById('increment');
 incrementButton.addEventListener('click', e => dispatch({ type: "INCREMENT" }));
+
+// Dispatch the "DECREMENT" action every time the +1 button is pressed
+const decrementButton = document.getElementById('decrement');
+decrementButton.addEventListener('click', e => dispatch({ type: "DECREMENT" }));
+
+// +5 button
+const addFiveButton = document.getElementById('add-five')
+addFiveButton.addEventListener('click', e => dispatch({ type: "ADD_5" }))
+
+// -5 button
+const subtractFiveButton = document.getElementById('subtract-five')
+subtractFiveButton.addEventListener('click', e => dispatch({ type: "SUBTRACT_5" }))
+
+// color select
+const colorSelectDropdown = document.getElementById('color-select')
+// colorSelectDropdown.addEventListener('change', e => dispatch({ type: e.target.value }))
+colorSelectDropdown.addEventListener('change', (e) => {
+  let newColor = e.target.value
+  dispatch ({
+    type: 'CHANGE_COLOR', newColor
+  })
+})
+
+// set new value
+const newValueField = document.getElementById('new-value')
+newValueField.addEventListener('keyup', (e) => {
+  if (e.keyCode === 13) {
+    let newValue = newValueField.value
+    newValue = parseInt(newValue, 10)
+    if (typeof newValue === 'number') {
+      dispatch ({
+        type: 'NEW_VALUE', newValue
+      })
+    }
+  }
+})
--- src/reducer.js
+++ src/reducer.js
@@ -1,5 +1,6 @@
 const initialState = {
-    value: 0
+    value: 0,
+    color: 'black'
 }
 
 const reducer = (state = initialState, action) => {
@@ -7,6 +8,28 @@ const reducer = (state = initialState, action) => {
     if (type === "INCREMENT") {
         return {
             value: state.value + 1
+        }
+    } else if (type === 'DECREMENT') {
+        return {
+            value: state.value - 1
+        }
+    } else if (type === 'ADD_5') {
+        return {
+            value: state.value + 5
+        }
+    } else if (type === 'SUBTRACT_5') {
+        return {
+            value: state.value - 5
+        }
+    } else if (type === 'CHANGE_COLOR') {
+        return {
+            value: state.value,
+            color: action.newColor
+        }
+    } else if (type === 'NEW_VALUE') {
+        return {
+            value: action.newValue,
+            color: state.color
         }
     }
     return state
